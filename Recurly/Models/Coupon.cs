68be6ee582529b4c7e2d2b96de7305892d04a353
Nicholas.Lechnowskyj <nick.l@identillect.com>
2014-12-26 14:58:22 -0800

    initial

---
+++ Recurly/Models/Coupon.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Serialization;
+using RestSharp.Deserializers;
+using RestSharp.Serializers;
+[assembly: InternalsVisibleTo("TestBench")]
+namespace Recurly
+{
+    [DeserializeAs(Name = "coupon")]
+    [SerializeAs(Name = "coupon")]
+    [XmlRoot("coupon")]
+    public class RecurlyCoupon
+    {
+        public enum CouponState : short
+        {
+            All = 0,
+            Redeemable,
+            Expired,
+            Inactive,
+            MaxedOut
+        }
+
+        public enum CouponDiscountType
+        {
+            [XmlEnum("percent")]
+            Percent,
+            [XmlEnum("dollars")]
+            Dollars
+        }
+
+        internal const string UrlPrefix = "coupons/";
+
+        [XmlIgnore]
+        public List<Redemption> Redemptions { get; internal set; }
+
+        [DeserializeAs(Name = "redemptions")]
+        [XmlIgnore]
+        public RecurlyRedemptionUrl RedemptionsUrl { get; internal set; }
+
+        /// <summary>
+        /// Unique code to identify and redeem the coupon. This code may only contain the following characters: [a-z A-Z 0-9 @ - _ .]. Max of 50 characters.
+        /// </summary>
+        [XmlElement("coupon_code")]
+        public string CouponCode { get; set; }
+
+        /// <summary>
+        /// Coupon name
+        /// </summary>
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of the coupon on the hosted payment pages
+        /// </summary>
+        [XmlElement("hosted_description")]
+        public string HostedDescription { get; set; }
+
+        /// <summary>
+        /// Description of the coupon on the invoice
+        /// </summary>
+        [XmlElement("invoice_description")]
+        public string InvoiceDescription { get; set; }
+
+        /// <summary>
+        /// Last date to redeem the coupon, defaults to no date
+        /// </summary>
+        [XmlElement("redeem_by_date")]
+        public DateTime? RedeemByDate { get; set; }
+
+        public bool RedeemByDateSpecified { get { return RedeemByDate.HasValue; } }
+
+        /// <summary>
+        /// If true, the coupon applies to the first invoice only
+        /// </summary>
+        [XmlElement("single_use")]
+        public bool SingleUse { get; set; }
+
+        /// <summary>
+        /// Number of months after redemption that the coupon is valid, defaults to no date
+        /// </summary>
+        [XmlElement("applies_for_months")]
+        public int? AppliesForMonths { get; set; }
+
+        public bool AppliesForMonthsSpecified { get { return AppliesForMonths.HasValue; } }
+
+        /// <summary>
+        /// Maximum number of accounts that may use the coupon before it can no longer be redeemed
+        /// </summary>
+        [XmlElement("max_redemptions")]
+        public int? MaxRedemptions { get; set; }
+
+        /// <summary>
+        /// The coupon is valid for all plans if true, defaults to true
+        /// </summary>
+        [XmlElement("applies_to_all_plans")]
+        public bool AppliesToAllPlans { get; set; }
+
+        [XmlElement("discount_type")]
+        public CouponDiscountType DiscountType { get; set; }
+
+        [XmlElement("state")]
+        [XmlIgnore]
+        public CouponState State { get; internal set; }
+
+        /// <summary>
+        /// A dictionary of currencies and discounts
+        /// </summary>
+        [XmlElement("discount_in_cents")]
+        public RecurlyCurrency DiscountInCents { get; set; }
+
+        /// <summary>
+        /// Discount percentage if discount_type is "percent"
+        /// </summary>
+        [XmlElement("discount_percent")]
+        public int? DiscountPercent { get; set; }
+        public bool DiscountPercentSpecified { get { return DiscountPercent.HasValue; } }
+
+        /// <summary>
+        /// A list of plans to limit the coupon
+        /// </summary>
+        [XmlElement("plan_codes", IsNullable = true)]
+        public List<string> Plans { get; set; }
+
+        [DeserializeAs(Name = "created_at")]
+        [XmlIgnore]
+        public DateTime CreatedAt { get; internal set; }
+
+        public RecurlyCoupon(string code, string name, RecurlyCurrency discounts)
+        {
+            Plans = new List<string>();
+            Redemptions = new List<Redemption>();
+            CouponCode = code;
+            Name = name;
+            DiscountInCents = discounts;
+            DiscountType = CouponDiscountType.Dollars;
+            AppliesToAllPlans = true;
+        }
+
+        public RecurlyCoupon(string code, string name, int discountPercent)
+        {
+            Plans = new List<string>();
+            Redemptions = new List<Redemption>();
+
+            CouponCode = code;
+            Name = name;
+            DiscountPercent = discountPercent;
+            DiscountType = CouponDiscountType.Percent;
+            AppliesToAllPlans = true;
+
+        }
+
+        public RecurlyCoupon() : this(null, null, 0) { }
+    }
+}
---
+++ Recurly/RecurlyClient.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+using Recurly.Configuration;
+using RestSharp;
+using RestSharp.Contrib;
+using RestSharp.Deserializers;
+using RestSharp.Serializers;
+
+namespace Recurly
+{
+    public partial class RecurlyClient
+    {
+        private readonly string BaseUrl = "https://{0}.recurly.com/v2/";
+        private readonly RestClient _client;
+
+        public RecurlyClient()
+        {
+            BaseUrl = string.Format(BaseUrl, Settings.Instance.Subdomain);
+            _client = new RestClient(BaseUrl);
+            _client.Authenticator = new HttpBasicAuthenticator(Settings.Instance.ApiKey, null);
+            _client.AddHandler("application/xml", new RecurlyXmlDeserializer());
+        }
+
+        private Task<IRestResponse<T>> GetResourceAsync<T>(string url) where T : new()
+        {
+            var restClient = new RestClient(url);
+            restClient.Authenticator = new HttpBasicAuthenticator(Settings.Instance.ApiKey, null);
+            var request = new RestRequest();
+
+            return Task<IRestResponse<T>>.Factory.StartNew(() => Execute<T>(request, restClient));
+        }
+
+        /// <summary>
+        /// A wrapper around RestSharp's Execute which checks and handles errors
+        /// </summary>
+        private IRestResponse<T> Execute<T>(IRestRequest request, RestClient client = null) where T : new()
+        {
+          //  request.XmlSerializer = new DotNetXmlSerializer("www.contonso.com");
+            var response = client == null ? _client.Execute<T>(request) : client.Execute<T>(request);
+            CheckForError(response);
+
+            return response;
+        }
+
+        /// <summary>
+        /// A wrapper around RestSharp's Execute which checks and handles errors
+        /// </summary>
+        private void Execute(IRestRequest request)
+        {
+          //  request.XmlSerializer = new DotNetXmlSerializer("www.contonso.com");
+            var response = _client.Execute(request);
+            CheckForError(response);
+        }
+
+        /// <summary>
+        /// A wrapper around RestSharp's Execute which checks and handles errors
+        /// </summary>
+        private Task<T> ExecuteAsync<T>(IRestRequest request) where T : new()
+        {
+            return Task.Factory.StartNew(() => Execute<T>(request).Data);
+        }
+
+        /// <summary>
+        /// A wrapper around RestSharp's Execute which checks and handles errors
+        /// </summary>
+        private Task ExecuteAsync(IRestRequest request)
+        {
+            return Task.Factory.StartNew(() => Execute(request));
+        }
+
+        private void CheckForError(IRestResponse response)
+        {
+            Error[] errors;
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                case HttpStatusCode.Accepted:
+                case HttpStatusCode.Created:
+                case HttpStatusCode.NoContent:
+                    return;
+
+                case HttpStatusCode.NotFound:
+                    errors = Error.ReadResponseAndParseErrors(response);
+                    if (errors.Length > 0) { throw new RecurlyNotFoundException(errors[0].Message, errors); }
+
+                    throw new RecurlyNotFoundException("The requested object was not found.", errors);
+
+                case HttpStatusCode.Unauthorized:
+                case HttpStatusCode.Forbidden:
+                    errors = Error.ReadResponseAndParseErrors(response);
+                    throw new RecurlyInvalidCredentialsException(errors);
+
+                case HttpStatusCode.PreconditionFailed:
+                    errors = Error.ReadResponseAndParseErrors(response);
+                    throw new RecurlyValidationException(errors);
+
+                case HttpStatusCode.ServiceUnavailable:
+                    throw new TemporarilyUnavailableException();
+
+                case HttpStatusCode.InternalServerError:
+                    errors = Error.ReadResponseAndParseErrors(response);
+                    throw new ServerException(errors);
+            }
+
+            if ((int)response.StatusCode == RecurlyValidationException.HttpStatusCode)
+            {
+                errors = Error.ReadResponseAndParseErrors(response);
+                throw new RecurlyValidationException(errors);
+            }
+
+            if (response.ErrorException != null)
+                throw response.ErrorException;
+        }
+    }
+}
