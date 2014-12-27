b24c15c24060da90f9f32f9fbac060c6cf1a2290
Nicholas Lechnowskyj <nickl2035@gmail.com>
2014-12-26 18:35:20 -0800

    Added summary to currency properties and modified how serialization is handled.

--- Recurly/Models/RecurlyCurrency.cs
+++ Recurly/Models/RecurlyCurrency.cs
@@ -9,22 +9,108 @@ using RestSharp.Deserializers;
 
 namespace Recurly
 {
-    public class RecurlyCurrency 
+    public class RecurlyCurrency : IXmlSerializable
     {
-        public int? USD { get; set; }
-        public int? AUD { get; set; }
-        public int? CAD { get; set; }
-        public int? EUR { get; set; }
-        public int? GBP { get; set; }
-        public int? CZK { get; set; }
-        public int? DKK { get; set; }
-        public int? HUF { get; set; }
-        public int? NOK { get; set; }
-        public int? NZD { get; set; }
-        public int? PLN { get; set; }
-        public int? SGD { get; set; }
-        public int? SEK { get; set; }
-        public int? CHF { get; set; }
-        public int? ZAR { get; set; }
+        /// <summary>
+        /// United States Dollars
+        /// </summary>
+        public int USD { get; set; }
+
+        /// <summary>
+        /// Australian Dollars
+        /// </summary>
+        public int AUD { get; set; }
+
+        /// <summary>
+        /// Canadian Dollars
+        /// </summary>
+        public int CAD { get; set; }
+
+        /// <summary>
+        /// Euros
+        /// </summary>
+        public int EUR { get; set; }
+
+        /// <summary>
+        /// British Pounds
+        /// </summary>
+        public int GBP { get; set; }
+
+        /// <summary>
+        /// Czech Korunas
+        /// </summary>
+        public int CZK { get; set; }
+
+        /// <summary>
+        /// Danish Krones
+        /// </summary>
+        public int DKK { get; set; }
+
+        /// <summary>
+        /// Hungarian Forints
+        /// </summary>
+        public int HUF { get; set; }
+
+        /// <summary>
+        /// Norwegian Krones
+        /// </summary>
+        public int NOK { get; set; }
+
+        /// <summary>
+        /// New Zealand Dollars
+        /// </summary>
+        public int NZD { get; set; }
+
+        /// <summary>
+        /// Polish Zloty
+        /// </summary>
+        public int PLN { get; set; }
+
+        /// <summary>
+        /// Singapore Dollars
+        /// </summary>
+        public int SGD { get; set; }
+
+        /// <summary>
+        /// Swedish Kronas
+        /// </summary>
+        public int SEK { get; set; }
+
+        /// <summary>
+        /// Swiss Francs
+        /// </summary>
+        public int CHF { get; set; }
+
+        /// <summary>
+        /// South African Rand
+        /// </summary>
+        public int ZAR { get; set; }
+        public XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            foreach (var prop in typeof(RecurlyCurrency).GetProperties())
+            {
+                var propValue = (int)prop.GetValue(this, null);
+                if (propValue != 0)
+                    writer.WriteElementString(prop.Name, propValue.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Returns a value that reflects whether any of the currencies have a value greater than 0.
+        /// </summary>
+        internal bool AnySpecified
+        {
+            get { return typeof (RecurlyCurrency).GetProperties().Any(x => (int)x.GetValue(this, null) > 0); }
+        }
     }
 }
